5bb0a66d6989c0232851ef713e6fd9915082bb7e
Torning, Fraser <Fraser_Torning@comcast.com>
2018-07-11 18:15:45 -0400

    finalized

---
+++ assets/js/config.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDp7jN23p34lcnljw3_Ti0YMIzi15xUJG4",
+    authDomain: "trainscheduler-285b7.firebaseapp.com",
+    databaseURL: "https://trainscheduler-285b7.firebaseio.com",
+    projectId: "trainscheduler-285b7",
+    storageBucket: "trainscheduler-285b7.appspot.com",
+    messagingSenderId: "749594151186"
+  };
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,12 @@
 console.log("main.js loaded");
 
+setTimeout(function(){
+    window.location.reload(1);
+}, 30000);
+
+var trainTable = $("#train-table");
+var trainFormSubmit = $("#add-train");
+
 // Initialize Firebase
 try {
     var config = firebaseConfig;
@@ -10,10 +17,81 @@ try {
     console.log('Failed to initialize database');
 }
 
-// Delete this out - just for reference
-var title = $('#title');
-var wink = $('#wink');
-title.css('text-align', 'center');
-wink.css('height', '30px');
-
 // Go nuts
+class Train {
+    constructor(name, destination, starting_time, frequency) {
+        this.name = name;
+        this.destination = destination;
+        this.starting_time = this.setParseTime(starting_time);
+        this.frequency = parseInt(frequency);
+        
+    }
+    setParseTime(time) {
+        try {
+            return moment.utc(time, 'HH:mm').format('HH:mm');
+
+        } catch {
+            alert('Cannot parse train time');
+        }
+    }
+
+    getNextArrivalTime() {
+        var current_time = moment().format("HH:mm");
+        var arrival_time = moment.utc(this.starting_time, 'HH:mm').add(this.frequency, 'm');
+        while (arrival_time <= moment.utc(current_time, "HH:mm")) {
+            arrival_time.add(this.frequency, 'm');
+        }
+
+        if (arrival_time.hour() > 24) {
+            return this.starting_time;
+        } else {
+            return arrival_time.format('hh:mm A');
+        }
+    }
+
+    getMinutesAway() {
+        var current_time = moment().format("HH:mm");
+        var nextArrival = moment.utc(this.getNextArrivalTime(), "hh:mm A");
+        return moment.utc(nextArrival, "hh:mm A").diff(moment.utc(current_time, "HH:mm"), 'm')
+    }
+
+    addToTable() {
+        var row = $('<tr>');
+        var trainName = $('<td>').text(this.name);
+        var destination = $('<td>').text(this.destination);
+        var frequency = $('<td>').text(this.frequency);
+        var nextArrival = $('<td>').text(this.getNextArrivalTime());
+        var minutesAway = $('<td>').text(this.getMinutesAway());
+        row.append([trainName, destination, frequency, nextArrival, minutesAway])
+        trainTable.append(row);
+    }
+
+    commit() {
+        db.ref('/train').push(this);
+    }
+
+}
+
+db.ref('train').on("child_added", function(snapshot) {
+    var record = snapshot.val()
+    var train = new Train(record.name, record.destination, 
+                        record.starting_time, record.frequency);
+    train.addToTable();
+});
+
+trainFormSubmit.on("click", function(event){
+    event.preventDefault();
+    var name = $('#train-name').val();
+    var destination = $('#destination-name').val();
+    var starting_time = $('#starting-time').val();
+    var frequency = $('#frequency').val();
+    try {
+        newTrain = new Train(name, destination, starting_time, frequency);
+        newTrain.commit()
+    } catch {
+        alert('One or more fields were not formatted correctly');
+    }
+});
+
+
+
